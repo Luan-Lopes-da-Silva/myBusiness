c3c38782f7e5db35584a8a22f97cc0da511170b6
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2024-07-08 18:26:42 -0300

    s

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ import Hotjar from '@hotjar/browser';
 import Script from "next/script";
 const siteId = 5052095;
 const hotjarVersion = 6;
-
+const measurementId = 'G-2RW4KC3BJP'
 export default function RootLayout({
   children
 }) {
@@ -16,23 +16,16 @@ export default function RootLayout({
     <html lang="en">
        <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-2RW4KC3BJP`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2RW4KC3BJP', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${measurementId}');
+    `}
+      </Script>
       <head/>
       <body className={inter.className}>{children}</body>
     </html>
