e8c88987ce0d974d3251176e1479b0b006908a4c
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2024-07-08 18:08:06 -0300

    Try of another way

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 import Hotjar from '@hotjar/browser';
+import Script from "next/script";
 const siteId = 5052095;
 const hotjarVersion = 6;
 
@@ -13,16 +14,27 @@ export default function RootLayout({
   Hotjar.init(siteId,hotjarVersion)
   return (
     <html lang="en">
+       <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2RW4KC3BJP`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2RW4KC3BJP', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <head/>
       <body className={inter.className}>{children}</body>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2RW4KC3BJP"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        
-        gtag('config', 'G-2RW4KC3BJP');
-      </script>
     </html>
     
   );
