03be391309f99a0aefda1e7b937e182485283b7d
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2024-07-12 11:41:23 -0300

    Reconfig gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 import GoogleAnalytics from '@/components/GoogleAnalytics'
 import { Metadata } from "next";
-
+import Script from "next/script";
+const measurementId = 'G-2RW4KC3BJP'
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -16,11 +17,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  
   return (
     <html lang="en">
-       <GoogleAnalytics/>
+       <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${measurementId}');
+    `}
+      </Script>
       <head/>
       <body className={inter.className}>{children}</body>
     </html>
